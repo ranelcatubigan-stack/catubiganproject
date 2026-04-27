e11dd02b6e1c48b0b008de2320cb6514e61a33ed
ranelcatubigan-stack <ranel.catubigan@gmail.com>
2026-04-28 03:25:57 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CategoryController;
 
 Route::get('/', function () {
-    return view('/products');
+    return redirect('/products');
 });
 Route::get('/products', [ProductController::class, 'index']);
 
