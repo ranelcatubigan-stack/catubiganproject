e9cc9cd5131019137ab89ccc6cea35bba5690d36
ranelcatubigan-stack <ranel.catubigan@gmail.com>
2026-04-28 03:22:22 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CategoryController;
 
+Route::get('/', function () {
+    return view('/products');
+});
 Route::get('/products', [ProductController::class, 'index']);
 
 Route::post('/products123', [ProductController::class, 'store']);
